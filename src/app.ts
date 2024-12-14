1b4638ba7f9b16a58cdea9059b2d1271e860c1c1
msetu1 <msetu5763@gmail.com>
2024-12-15 02:41:48 +0600

    Project setup done

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+const app:Application= express();
+
+// parser 
+app.use(express.json());
+app.use(cors());
+
+// application route 
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+async function main() {
+    
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+        console.log(`Bikes Store project on port ${config.port}`)
+      })
+  } catch (error) {
+    console.error(error);
+  }
+  }
+  main()
